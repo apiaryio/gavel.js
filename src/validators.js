2e34392ada519c6810f822100e1fe1659a6ce514
Artem Zakharchenko <artem.zakharchenko@oracle.com>
2019-04-11 13:56:35 +0200

    refactor: port validate, validators, models and mixins to JavaScript

---
+++ src/validators.js
@@ -0,0 +1,7 @@
+module.exports = {
+  HeadersJsonExample: require('./validators/headers-json-example')
+    .HeadersJsonExample,
+  TextDiff: require('./validators/text-diff').TextDiff,
+  JsonExample: require('./validators/json-example').JsonExample,
+  JsonSchema: require('./validators/json-schema').JsonSchema,
+}
---
+++ src/validators/text-diff.js
@@ -0,0 +1,77 @@
+const type = require('is-type')
+const errors = require('../errors')
+const DiffMatchPatch = require('googlediff')
+// const { ValidationErrors } = require('./validation-errors')
+
+class TextDiff {
+  constructor(real, expected) {
+    if (typeof real !== 'string') {
+      const outError = new errors.DataNotStringError(
+        'String validator real: input data is not string',
+      )
+      outError.data = real
+      throw outError
+    }
+
+    if (typeof expected !== 'string') {
+      outError = new errors.DataNotStringError(
+        'String validator expected: input data is not string',
+      )
+      outError.data = expected
+      throw outError
+    }
+
+    this.real = real
+    this.expected = expected
+  }
+
+  validate() {
+    const sanitizeSurrogatePairs = (data) => {
+      return data
+        .replace(/[\uD800-\uDBFF]/g, '')
+        .replace(/[\uDC00-\uDFFF]/g, '')
+    }
+
+    this.output = null
+    const dmp = new DiffMatchPatch()
+
+    try {
+      const patch = dmp.patch_make(this.real, this.expected)
+      this.output = dmp.patch_toText(patch)
+      return this.output
+    } catch (error) {
+      if (error instanceof URIError) {
+        const patch = dmp.patch_make(
+          sanitizeSurrogatePairs(this.real),
+          sanitizeSurrogatePairs(this.expected),
+        )
+        this.output = dmp.patch_toText(patch)
+        return this.output
+      } else {
+        throw error
+      }
+    }
+  }
+
+  evaluateOutputToResults(data) {
+    if (!data) {
+      data = this.output
+    }
+
+    if (!data) {
+      return []
+    }
+
+    return [
+      {
+        severity: 'error',
+        /**
+         * @todo Include the diff?
+         */
+        message: 'Real and expected data do not match.',
+      },
+    ]
+  }
+}
+
+module.exports = { TextDiff }
