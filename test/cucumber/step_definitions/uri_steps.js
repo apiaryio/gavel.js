ad30890d66066269e4922f74834f26862d9859bf
artem-zakharchenko <artem.zakharchenko@oracle.com>
2019-06-20 14:47:30 +0200

    fix: removes arrow functions to prevent context binding of Cucumber steps

--- test/cucumber/step_definitions/uri_steps.js
+++ test/cucumber/step_definitions/uri_steps.js
@@ -1,10 +1,16 @@
 module.exports = function() {
-  this.Given(/^you expect HTTP message URI "([^"]*)"$/, (uri, callback) => {
+  this.Given(/^you expect HTTP message URI "([^"]*)"$/, function(
+    uri,
+    callback
+  ) {
     this.expected.uri = uri;
     return callback();
   });
 
-  return this.When(/^real HTTP message URI is "([^"]*)"$/, (uri, callback) => {
+  return this.When(/^real HTTP message URI is "([^"]*)"$/, function(
+    uri,
+    callback
+  ) {
     this.real.uri = uri;
     return callback();
   });
