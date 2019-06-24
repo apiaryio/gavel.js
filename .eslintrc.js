cbfb18c2dbb9856f00e7458dcd6f940d332168e0
artem-zakharchenko <artem.zakharchenko@oracle.com>
2019-06-24 11:19:55 +0200

    chore: separates temporary and permanent overrides in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,9 +12,12 @@ module.exports = {
     'no-plusplus': 'off',
     'func-names': 'off',
 
+    // Disabled to allow "expect()" assertions with parameters:
+    // expect(foo).to.be.valid
+    'no-unused-expressions': 'off',
+
     // Temporary overrides. Logic to be rewritten.
     // TODO https://github.com/apiaryio/gavel.js/issues/150
-    'no-param-reassign': 'off',
-    'no-unused-expressions': 'off'
+    'no-param-reassign': 'off'
   }
 };
