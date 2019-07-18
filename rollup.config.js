e165a7ffca04f4b7dee8ddd2060e844999a0bbbc
artem-zakharchenko <artem.zakharchenko@oracle.com>
2019-07-18 14:10:29 +0200

    fix: builds gavel using rollup

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+const resolve = require('rollup-plugin-node-resolve');
+const commonjs = require('rollup-plugin-commonjs');
+const packageJson = require('./package.json');
+
+const buildUmd = {
+  input: 'lib/index.js',
+  output: {
+    file: packageJson.main,
+    format: 'umd',
+    name: 'gavel',
+    exports: 'named',
+    sourcemap: true
+  },
+  plugins: [
+    resolve({
+      browser: true,
+
+      // Forbid bundling of NodeJS built-ins (i.e. "fs", "path").
+      // Throw when such modules are present in the bundle.
+      preferBuiltins: false
+    }),
+    commonjs()
+  ]
+};
+
+module.exports = [buildUmd];
