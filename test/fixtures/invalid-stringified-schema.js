3aade08ea1fe67109424d7a9aa2598d38aa289c0
artem-zakharchenko <artem.zakharchenko@oracle.com>
2019-08-14 13:01:25 +0200

    chore: removes jju mentions

--- test/fixtures/invalid-stringified-schema.js
+++ test/fixtures/invalid-stringified-schema.js
@@ -1,4 +1,1 @@
-// Note that Json Schema now uses "jju" for JSON parsing
-// which allows certain JSON abnormalities (i.e. trailing commas, single quotes).
-// Account on that to produce an invalid JSON.
 module.exports = `{"111!invalid string ... issue":malformed''}`;
